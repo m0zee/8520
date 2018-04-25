276729a1392dd787aef2e19715a1a3b4ceeeebcf
Sawood <mrsaudahmed@hotmail.com>
2018-04-25 17:29:07 +0500

    commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -228,7 +228,7 @@
                 <div class="call-to-wrap">
                     <h1 class="text--white">Ready to Join Our Marketplace!</h1>
                     <h4 class="text--white">Over 25,000 designers and developers trust the MartPlace.</h4>
-                    <a href="#" class="btn btn--lg btn--round btn--white callto-action-btn">Join Us Today</a>
+                    <a href="{{ route('register') }}" class="btn btn--lg btn--round btn--white callto-action-btn">Join Us Today</a>
                 </div>
             </div>
         </div>
