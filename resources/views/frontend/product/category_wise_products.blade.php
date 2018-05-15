5d497058f70d3439e679621fda398aa626c8c149
Sawood <mrsaudahmed@hotmail.com>
2018-05-15 16:59:43 +0500

    update

--- resources/views/frontend/product/category_wise_products.blade.php
+++ resources/views/frontend/product/category_wise_products.blade.php
@@ -36,57 +36,7 @@
                             </div><!-- end /.collapsible_content -->
                         </div><!-- end /.sidebar-card -->
 
-                        <div class="sidebar-card card--filter">
-                            <a class="card-title" href="#collapse2" role="button" data-toggle="collapse"  aria-expanded="false" aria-controls="collapse2">
-                                <h4>Filter Products<span class="lnr lnr-chevron-down"></span></h4>
-                            </a>
-                            <div class="collapse in collapsible-content" id="collapse2">
-                                <ul class="card-content">
-                                <li>
-                                    <div class="custom-checkbox2">
-                                        <input type="checkbox" id="opt1" name="filter_opt"> <label for="opt1"><span class="circle">
-                                        </span>Trending Products</label>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="custom-checkbox2">
-                                        <input type="checkbox" id="opt2" name="filter_opt"> <label for="opt2"><span class="circle">
-                                        </span>Popular Products</label>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="custom-checkbox2">
-                                        <input type="checkbox" id="opt3" name="filter_opt"> <label for="opt3"><span class="circle">
-                                        </span>New Products</label>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="custom-checkbox2">
-                                        <input type="checkbox" id="opt4" name="filter_opt"> <label for="opt4"><span class="circle">
-                                        </span>Best Seller</label>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="custom-checkbox2">
-                                        <input type="checkbox" id="opt5" name="filter_opt"> <label for="opt5"><span class="circle">
-                                        </span>Best Rating</label>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="custom-checkbox2">
-                                        <input type="checkbox" id="opt6" name="filter_opt"> <label for="opt6"><span class="circle">
-                                        </span>Free Support</label>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="custom-checkbox2">
-                                        <input type="checkbox" id="opt7" name="filter_opt"> <label for="opt7"><span class="circle">
-                                        </span>On Sale</label>
-                                    </div>
-                                </li>
-                            </ul>
-                            </div>
-                        </div><!-- end /.sidebar-card -->
+                        <!-- end /.sidebar-card -->
     
                        {{--  <div class="sidebar-card card--slider">
                                 <a class="card-title" href="#collapse3" role="button" data-toggle="collapse"  aria-expanded="false" aria-controls="collapse3">
--- resources/views/frontend/product/index.blade.php
+++ resources/views/frontend/product/index.blade.php
@@ -41,64 +41,6 @@
                             </div><!-- end /.collapsible_content -->
                         </div><!-- end /.sidebar-card -->
 
-                        <div class="sidebar-card card--filter">
-                            <a class="card-title" href="#collapse2" role="button" data-toggle="collapse"  aria-expanded="false" aria-controls="collapse2">
-                                <h4>Filter Products<span class="lnr lnr-chevron-down"></span></h4>
-                            </a>
-                            <div class="collapse in collapsible-content" id="collapse2">
-                                <ul class="card-content">
-                                    <li>
-                                        <div class="custom-checkbox2"><input type="checkbox" id="opt1" class="" name="filter_opt"> 
-                                            <label for="opt1">
-                                                <span class="circle"></span> Trending Products
-                                            </label>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="custom-checkbox2"><input type="checkbox" id="opt2" class="" name="filter_opt"> 
-                                            <label for="opt2">
-                                                <span class="circle"></span> Popular Products
-                                            </label>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="custom-checkbox2"><input type="checkbox" id="opt3" class="" name="filter_opt"> 
-                                            <label for="opt3">
-                                                <span class="circle"></span> New Products
-                                            </label>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="custom-checkbox2"><input type="checkbox" id="opt4" class="" name="filter_opt"> 
-                                            <label for="opt4">
-                                                <span class="circle"></span> Best Seller
-                                            </label>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="custom-checkbox2"><input type="checkbox" id="opt5" class="" name="filter_opt"> 
-                                            <label for="opt5">
-                                                <span class="circle"></span> Best Rating
-                                            </label>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="custom-checkbox2"><input type="checkbox" id="opt6" class="" name="filter_opt"> 
-                                            <label for="opt6">
-                                                <span class="circle"></span> Free Support
-                                            </label>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="custom-checkbox2"><input type="checkbox" id="opt7" class="" name="filter_opt"> 
-                                            <label for="opt7">
-                                                <span class="circle"></span> On Sale
-                                            </label>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div><!-- end /.sidebar-card -->
                     </aside><!-- end aside -->
 
                 </div><!-- end /.col-md-3 -->
